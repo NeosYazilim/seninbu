00ccc8d4c64404c0a89e7ec80727247daa15f21a
NeosYazilim <yazilimneos2@gmail.com>
2021-12-15 12:21:44 +0300

    pages modified

--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,17 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about-us', views.about_us, name='about-us'),
+    path('contact-us', views.contact_us, name='contact_us'),
+    path('faq', views.faq, name='faq'),
+    path('login', views.login, name='login'),
+    path('cart', views.cart, name='cart'),
+    path('checkout', views.checkout, name='checkout'),
+    path('order', views.order, name='order'),
+    path('product-360-degree', views.product_360_degree, name='product-360-degree'),
+    path('products', views.product, name='product'),
+    path('shop', views.shop, name='shop'),
+    path('store-listing', views.store_listing, name='store-listing'),
+    path('wishlist', views.wishlist, name='wishlist'),
    
 ]
--- product/views.py
+++ product/views.py
@@ -2,3 +2,27 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html')
+def about_us(request):
+    return render(request,'about-us.html')
+def contact_us(request):
+    return render(request,'contact-us.html')
+def faq(request):
+    return render(request,'faq.html')
+def login(request):
+    return render(request,'login.html')
+def cart(request):
+    return render(request,'cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
+def order(request):
+    return render(request,'order.html')
+def product_360_degree(request):
+    return render(request,'product-360-degree.html')
+def product(request):
+    return render(request,'product.html')
+def shop(request):
+    return render(request,'shop.html')
+def store_listing(request):
+    return render(request,'store-listing.html')
+def wishlist(request):
+    return render(request,'wishlist.html')
